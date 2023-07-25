a29063189203b3d3be2a502630fe29bfc7c9ec40
eyasu <mesakmegerem@gmail.com>
2023-07-25 07:47:54 -0700

    out to test folder

--- _putchar.c
+++ _putchar.c
@@ -1,12 +1,14 @@
 #include "main.h"
 
 /**
-* _putchar - Entry point...
-* Description: 'the program desc'
-* Return: Always 0 (Success)
-* @c: char parmameterr
-*/
-int _putchar(char c)
+ * _putchar - prints a single character
+ *
+ * @c: character to be printed
+ *
+ * Return: 0 on success, 1 otherwise
+ */
+
+int _putchar(int c)
 {
 	return (write(1, &c, 1));
 }
