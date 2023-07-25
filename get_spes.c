47a788d3063114e3d6c996ed3b56a33ed69277ab
eyasu <mesakmegerem@gmail.com>
2023-07-25 14:40:05 -0700

    fix struct

--- get_spes.c
+++ get_spes.c
@@ -12,21 +12,22 @@
 
 int (*get_spes(const char *format, int index))(va_list args)
 {
-	int i;
-	struct op ops[] = {
-		{"s", _str}, {"i", int_print}, {"o", _oct},
-		{"c", _c}, {"d", int_print}, {"x", hexprint},
-		{"%", _perc}, {"u", un_int}, {"X", heXprint},
-		{"b", un_bin}, {NULL, NULL}
-	};
+        int i;
+        struct op ops[] = {
+                {"s", _str}, {"i", int_print},
+                {"c", _c}, {"d", int_print},
+                {"%", _perc}, {"u", un_int},
+                {NULL, NULL},
+        };
 
-	i = 0;
+        i = 0;
 
-	while (ops[i].choice != NULL)
-	{
-		if (ops[i].choice[0] == format[index])
-			return (ops[i].f);
-		i++;
-	}
-	return (NULL);
+        while (ops[i].choice != NULL)
+        {
+                if (ops[i].choice[0] == format[index])
+                        return (ops[i].f);
+                i++;
+        }
+        return (NULL);
 }
+
