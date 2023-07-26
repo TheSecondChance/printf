7c0dea3132ff20a4834680c341e735d51a6e57a4
eyasu <mesakmegerem@gmail.com>
2023-07-25 18:21:39 -0700

    modifiy

--- main.h
+++ main.h
@@ -18,15 +18,15 @@ struct list
 };
 
 void print_unsign(int num);
-int _pchar(int c);
 int _printf(const char *format, ...);
+int _pchar(int c);
 int print_integer(va_list);
-int (*check_value(const char *, int))(va_list);
 int print_str(va_list);
-int char_new(va_list);
 int print_perc(va_list);
+int (*check_value(const char *, int))(va_list);
 int int_new(int);
 int unsign_int(va_list);
+int char_new(va_list);
 
 #endif
 
