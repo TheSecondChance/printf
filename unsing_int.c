4bd2a1f3e91f2c54896d46e44f73a7201482b840
eyasu <mesakmegerem@gmail.com>
2023-07-25 17:59:30 -0700

    fix

--- unsing_int.c
+++ unsing_int.c
@@ -1,30 +1,12 @@
 #include "main.h"
 /**
- * unsign_int - print unsigned integer
- * @args: input integer maybe
- * Return: count printed
+ * print_unsign - prints unsinged integer.
+ * @num: integer paramater.
  */
 
-int unsign_int(va_list args)
+void print_unsign(int num)
 {
-	unsigned int num;
-	int count = 0;
-	int n;
-
-	n = va_arg(args, int);
-
-	if (n < 0)
-	{
-		num = UINT_MAX + (n + 1);
+	if (num / 10)
 		print_unsign(num / 10);
-		_pchar(num % 10 + 48);
-		count++;
-	}
-	else
-	{
-		if (n / 10)
-			int_new(n / 10);
-		_pchar(n % 10 + '0');
-	}
-	return (count);
+	_pchar(num % 10 + 48);
 }
